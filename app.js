faaa544b243886faf303939229aa649c9d3c368f
HZ-2000 <delta-z@outlook.com>
2021-10-24 11:49:30 -0700

    Added Resume PDF

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+const path = require("path");
+const fs = require("fs");
 const port = 3000
+const files = fs.readdirSync("public/pdfs").map(name => {
+  return {
+    name: path.basename(name, ".pdf"),
+    url: `/pdfs/${name}`
+  };
+});
 
 //static files
 app.use(express.static('public'))
 app.use(express.json())
 app.use("/css", express.static(__dirname + 'public/css'))
+
+app.use(
+  express.static("public", {
+    setHeaders: (res, filepath) =>
+      res.attachment(`pdf-express-${path.basename("public/Resume.pdf")}`)
+  })
+);
 
 app.set('view engine', 'ejs')
 
@@ -26,6 +41,10 @@ app.get('/pomodoro', (req, res) => {
 })
 
 app.get('/contact', (req, res) => {
+  res.render("contact", { files })
+})
+
+app.get('/Resume', (req, res) => {
   res.render("contact")
 })
 
